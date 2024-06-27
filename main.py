b4880547b6d09828111d0f0e8e692afec2a28436
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 16:39:08 -0700

    List and Total Comlpete

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ def main():
    
     total = 0
     i = 0
-    numbers = []
+    numbers = [0] * 5
     while i < 5:
-        numbers = random.randint(0, 100)
-        total = total + numbers[i]
+        numbers[i] = random.randint(0, 100)
+        total = sum(numbers)
         i += 1
-    
     
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
